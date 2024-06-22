0f3d58f676d0a447ee8de1d4c43544a923d0b132
Richard Dobler <richarddobler7@outlook.de>
2024-06-22 23:20:12 +0200

    added statements for selecting a drink

--- main.cpp
+++ main.cpp
@@ -11,51 +11,85 @@ DigitalOut in3(PC_8);
 DigitalOut in2(PC_6);
 DigitalOut in1(PC_5);
 DigitalOut led(PC_0);
-DigitalIn taster(PC_11);
+DigitalIn tasterMenu(PC_11);
+DigitalIn tasterSelected(PC_10);
 lcd mylcd;
-int i = 0;
-void clickTaster(){
-    switch (i) {
+int menuNumber = 0;
+int drinkSelected = 0;
+void clickTasterMenu() {
+  switch (menuNumber) {
 
-    case 0:
-      mylcd.clear();
-      mylcd.cursorpos(0);
-      mylcd.printf("Wodkashot");
-      // printf("wodkashot");
-        thread_sleep_for(100);
-        i++;
-      break;
-    case 1:
-      mylcd.cursorpos(0);
-      mylcd.printf("Wodkashot2");
-      thread_sleep_for(100);
-      i++;
-      break;
-    case 2:
-      mylcd.cursorpos(0);
-      mylcd.printf("Wodkashot3");
-      thread_sleep_for(100);
-      i = 0;
-      
-      break;
-      
+  case 0:
+    mylcd.clear();
+    mylcd.cursorpos(0);
+    mylcd.printf("Wodkashot");
+    // printf("wodkashot");
+    thread_sleep_for(100);
+    menuNumber++;
+    drinkSelected = 0;
+    break;
+  case 1:
+    mylcd.cursorpos(0);
+    mylcd.printf("Wodkashot2");
+    thread_sleep_for(100);
+    menuNumber++;
+    drinkSelected = 1;
+    break;
+  case 2:
+    mylcd.cursorpos(0);
+    mylcd.printf("Wodkashot3");
+    thread_sleep_for(100);
+    menuNumber = 0;
+    drinkSelected = 2;
+    break;
+  }
+}
+void clickSelected(){
+    if (drinkSelected==0) {
+        in3 = 0;
+        in4 = 0;
+        in1 = 0; // forward out 1 & 2
+        in2 = 1; // forward in1=0 in2=1 out 1 & 2
+        printf("motor1 rennt: %d\n ",drinkSelected);
     }
-
+    if (drinkSelected==1) {
+        in1 = 0;
+        in2 = 0;
+        in3 = 1;
+        in4 = 0;
+        printf("motor2 rennt: %d\n",drinkSelected);
+    }
+    if (drinkSelected==2) {
+        in1 = 0; // forward out 1 & 2
+        in2 = 1;
+        in3 = 1;
+        in4 = 0;
+        printf("beide rennt %d\n",drinkSelected);
+    }
+    thread_sleep_for(500);
 }
 int main() {
+  tasterSelected.mode(PullDown);
+  tasterMenu.mode(PullDown);
+  printf("Int i= %d\n",menuNumber);
   mylcd.clear();
-  clickTaster();
+  mylcd.printf("click button");
+  //clickTasterMenu();
   
-  //taster.mode(PullDown);
-  // input.mode(PullDown);
-       // Dispalyobjekt erstellen (kann auch global deklariert werden)
-  
-
-
   while (true) {
-    if (taster==true) {
-        clickTaster();
+    if (tasterMenu==true) {
+        printf("auswahl\n");
+        printf("Int i= %d\n",menuNumber);
+        clickTasterMenu();
   }
+    if (tasterSelected==true) {
+        printf("motor laufen\n");
+        printf("Int i= %d\n",menuNumber);
+        clickSelected();
+    }
+    else {
+    //printf("idle\n");
+    }
 
   } // end while
 } // end mai
